14b5266eec8db5e7b2b34fba3fb5abac20dd8f24
chedhly <154672269+chedhly@users.noreply.github.com>
2025-10-18 17:25:28 +0100

    Updated build.gradle.kts

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -17,6 +17,11 @@ subprojects {
 }
 subprojects {
     project.evaluationDependsOn(":app")
+    afterEvaluate {
+        if (project.name == "google_mlkit_commons") {
+            project.android.namespace = "com.google.mlkit.common"
+        }
+    }
 }
 
 tasks.register<Delete>("clean") {
